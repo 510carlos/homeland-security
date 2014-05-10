bc0383555fad3a20e86553533ef32a2b8da069a3
510carlos <carlos.mendoza.91@gmail.com>
2014-05-10 11:43:11 -0700

    Made files + cse model

---
+++ application/models/case_model.php
@@ -0,0 +1,30 @@
+class Case_model extends CI_Model {
+	
+    var $CaseID;
+    var $operation;
+    var $enocoded_data;
+    
+    function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();
+    }
+    
+    function getCase($caseID) 
+    {
+    	
+    }
+    
+    function addCase($operation, $encoded_data)
+    {
+    	$this->operation = $opearation;
+        $$this->encoded_data = $encoded_data;
+        
+        // look CI insert
+        $query = $this->db->insert('entries', 10);
+        
+        $this->caseID = $case->caseID;
+        return $query->result();
+    }
+
+}
