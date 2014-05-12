f8af7cbad226731efaa281c80ac19204a05a2a8e
510carlos <carlos_m_91@yahoo.com>
2014-05-11 20:02:59 -0700

    Update department_model.php

--- application/models/department_model.php
+++ application/models/department_model.php
@@ -1,19 +1,43 @@
 class Department_model extends CI_Model {
 
-    var $title   = '';
-    var $content = '';
-    var $date    = '';
-
+    var $department;
+    
     function __construct()
     {
         // Call the Model constructor
         parent::__construct();
     }
     
-    function addDepartment () {}
+    function addDepartment ($username, $password, $name, $address, $social, $email, $dob) 
+    {
+        $data = array(
+            'username'  => $username,
+            'password'  => $password,
+            'name'      => $name,
+            'address'   => $address,
+            'social'    => $social,
+            'email'     => $email,
+            'DoB'       => $dob,
+            'Date'      => time(),
+        );
+        $this->db->insert('departments', $data);
+        $this->user = $this->getUser($this->db->insert_id());
+    }
     
-    function hideDepartment () {}
+    function hideDepartment () 
+    {
+        $data = array('hide' => 1);
+    	$this->db->where('userid', $caseID);
+        $this->db->update('departments', $data); 
     
-    function getDepartment () {}
-
+    }
+    
+    function getDepartment () 
+    {
+        $sql = $this->db->from('departments')->where('userid', $userid)->get();
+        $data = $sql->row();
+        
+        return $data;
+    }
+    
 }
