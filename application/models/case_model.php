9222b464f73517c2aba723dfcb742c4b826a558b
510carlos <carlos_m_91@yahoo.com>
2014-05-11 19:44:11 -0700

    updated case_model

--- application/models/case_model.php
+++ application/models/case_model.php
@@ -12,22 +12,38 @@ class Case_model extends CI_Model {
     
     function getCase($caseID) 
     {
-    	
+    	$sql = $this->db->from('case'')->where('caseID', $caseID)->get()
+    	$data = $sql->row()
+    	return $data;
     }
+    
     
     function addCase($operation, $encoded_data)
     {
     	$this->operation = $opearation;
-        $$this->encoded_data = $encoded_data;
+        $this->encoded_data = $encoded_data;
+        
+        $data = array(
+           'operation' => $operation,
+           'data'  =>  jsone_encode($_POST),
+           'hide'  => 0,
+        );
+        
         
         // look CI insert
-        $query = $this->db->insert('entries', 10);
+        $query = $this->db->insert('cases', $data);
         
         $this->caseID = $case->caseID;
         return $query->result();
     }
 
-	function hideCase() {}
+    function hideCase($caseID) 
+    {
+    	$data = array('hide' => 1);
+    	$this->db->where('caseid', $caseID);
+        $this->db->update('casses', $data); 
+
+    }
     
     
 }
