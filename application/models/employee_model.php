322b94f422a382e9721900dbe9705db5b8a951e7
510carlos <carlos_m_91@yahoo.com>
2014-05-11 20:00:19 -0700

    Update employee_model.php

--- application/models/employee_model.php
+++ application/models/employee_model.php
@@ -10,11 +10,39 @@ class Employee_model extends CI_Model {
         parent::__construct();
     }
     
-    function addEmployee () {}
+
     
-    function hideEmployee () {}
+    function addEmployee ($username, $password, $name, $address, $social, $email, $dob) 
+    {
+        $data = array(
+            'username'  => $username,
+            'password'  => $password,
+            'name'      => $name,
+            'address'   => $address,
+            'social'    => $social,
+            'email'     => $email,
+            'DoB'       => $dob,
+            'Date'      => time(),
+        );
+        $this->db->insert('users', $data);
+        $this->user = $this->getEmployee($this->db->insert_id());
+    }
     
-    function getEmployee () {}
+    function hideEmployee () 
+    {
+        $data = array('hide' => 1);
+    	$this->db->where('userid', $caseID);
+        $this->db->update('users', $data); 
+    
+    }
+    
+    function getEmployee () 
+    {
+        $sql = $this->db->from('users')->where('userid', $userid)->get();
+        $data = $sql->row();
+        
+        return $data;
+    }
     
 
 }
