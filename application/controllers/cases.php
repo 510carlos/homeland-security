2035224bd500de095bf62dddd1f87a46fce64139
510carlos <carlos.mendoza.91@gmail.com>
2014-05-14 20:36:40 -0700

    Controlles + cases

---
+++ application/controllers/cases.php
@@ -0,0 +1,43 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Cases extends CI_Controller {
+
+	public function index()
+	{
+	
+		
+		if($_POST) {
+			
+			$data = array();
+			
+			$form_id = (int) $this->input->post('form_id');
+			foreach($_POST as $key => $p) {
+				$match_prefix = "DHS_";
+				$prefix = substr($p, 0,3);	
+				if($match_prefix == $prefix) {
+					$data[$key] = $p;
+				}
+			}
+			
+			$insert = array(
+				'form_id'	=> $form_id,
+				'data'		=> json_encode($data),
+			);
+			
+			var_dump($data);
+			exit;
+			//$this->db->where('form_id', $form_id)->insert('cases', $data);
+		}
+		
+		
+		$this->load->view('forms');
+		
+		
+	}
+	
+	public function form ()
+	{
+		
+	}
+}
+
